0e11d6f7cd70384d84b2a9e1c6cdf83d9a0dc271
Nandiniii28 <singhnandini9358@gmail.com>
2025-07-01 15:56:59 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,10 @@ import HostingFeatureArea from "@/components/ui/hostingfeaturearea";
 import SecureManagement from "@/components/ui/securemanagement";
 import EliteHostFeatureArea from "@/components/ui/elitehostingfeaturearea";
 import Testimonials from "@/components/ui/testimonials";
+import LatestBlog from "@/components/ui/latestblog";
+import HostingFaq from "@/components/ui/hostingfaq";
+import Newsletter from "@/components/ui/newsletter";
+
 
 export default function Home() {
   return (
@@ -18,6 +22,9 @@ export default function Home() {
       <SecureManagement />
       <EliteHostFeatureArea />  
       <Testimonials />
+      <LatestBlog />
+      <HostingFaq />
+      <Newsletter />
     </>
   );
 }
